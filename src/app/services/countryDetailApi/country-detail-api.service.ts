257f81aa0d3a5a4d9479dd1b9c0ef8ae07fe176b
Luismi Sánchez <lmsanchez@esferize.com>
2021-01-31 10:56:50 +0100

    Service using Adpater: https://florimond.dev/blog/articles/2018/09/consuming-apis-in-angular-the-model-adapter-pattern/

--- src/app/services/countryDetailApi/country-detail-api.service.ts
+++ src/app/services/countryDetailApi/country-detail-api.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { Country, CountryAdapter } from '../../components/country/models/country.model';
+import {HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import {map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CountryDetailApiService {
 
-  constructor() { }
+  private baseUrl = 'https://restcountries.eu/rest/v2/alpha/es';
+
+  constructor(
+    private http: HttpClient,
+    private adapter: CountryAdapter
+  ) {}
+
+  list(): Observable<Country[]> {
+    const url = `${this.baseUrl}/`;
+    return this.http.get(url).pipe(
+      // Adapt each item in the raw data array
+      map((data: any[]) => data.map((item) => this.adapter.adapt(item)))
+    );
+  }
+
 }
