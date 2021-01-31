8765752ff9f9ec64812b1be1d343601a839af13a
Luismi Sánchez <lmsanchez@esferize.com>
2021-01-31 18:31:26 +0100

    styles and country list service

--- src/app/components/country-list/country-list.component.ts
+++ src/app/components/country-list/country-list.component.ts
@@ -1,24 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { CountryListApiService } from '../../services/countryListApi/country-list-api.service';
 
 @Component({
   selector: 'app-country-list',
   templateUrl: './country-list.component.html',
-  styleUrls: ['./country-list.component.scss']
+  styleUrls: ['./country-list.component.scss'],
+  providers: [CountryListApiService]
 })
 export class CountryListComponent implements OnInit {
 
   fillerNav = Array.from({length: 10}, (_, i) => `Nav Item ${i + 1}`);
   public searchCountry: string;
+  public countryList: Array<any>;
 
-
-
-  constructor() {
+  constructor(private countries: CountryListApiService) {
 
     this.searchCountry = 'Start typing a country name';
 
   }
 
   ngOnInit(): void {
+
+    // console.log(this.getCountries());
+    this.getCountries();
+
   }
 
+  getCountries() {
+    this.countries.getData()
+      .subscribe(
+        data => {
+          this.countryList = data;
+          // console.log(data);
+          // return data;
+        },
+        error => {
+          console.log(error);
+        }
+      );
+  }
 }
--- src/app/services/countryListApi/country-list-api.service.ts
+++ src/app/services/countryListApi/country-list-api.service.ts
@@ -1,9 +1,65 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpErrorResponse, HttpHeaders} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry, map } from 'rxjs/operators';
+
+// Set the http options
+const httpOptions = {
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  // headers: new HttpHeaders({ 'Content-Type': 'application/json'})
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class CountryListApiService {
 
-  constructor() { }
+  private url: string;
+
+  constructor(private http: HttpClient) {
+    this.url = 'https://restcountries.eu/rest/v2/all?fields=name;alpha2Code';
+  }
+
+
+  /**
+   * Function to GET list of countries
+   */
+  public getData(): Observable<any> {
+
+    // Call the http GET
+    return this.http.get(this.url, httpOptions).pipe(
+      map(this.extractData),
+      catchError(this.handleError)
+    );
+
+  }
+
+  /**
+   * Function to handle error when the server return an error
+   *
+   * @param error
+   */
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code. The response body may contain clues as to what went wrong,
+      console.error(
+        `Backend returned code ${error.status}, ` + `body was: ${error.error}`
+      );
+    }
+    // return an observable with a user-facing error message
+    return throwError(error);
+  }
+
+  /**
+   * Function to extract the data when the server return some
+   *
+   * @param res
+   */
+  private extractData(res: Response) {
+    return res || {};
+  }
+
 }
